86d5b1c002c6a704cb5b4378661c99144c857d03
gdcdoma-hash <gdcdoma@gmail.com>
2026-08-16 10:24:28 -0300

    test: fix pgTAP assertion plan

--- platform/supabase/tests/0057_migration_challenge_insert_or_verify.sql
+++ platform/supabase/tests/0057_migration_challenge_insert_or_verify.sql
@@ -1,7 +1,7 @@
 begin;
 create extension if not exists pgtap with schema extensions;
 set local search_path = public, extensions;
-select plan(9);
+select plan(10);
 
 select has_function(
   'public',
